1c237ba64470fe70c88e39489905250f6f0a01de
MrXonee <114215468+MrXonee@users.noreply.github.com>
2022-09-23 22:03:29 +0700

    Add files via upload

---
+++ it.sh
@@ -0,0 +1,243 @@
+#!/bin/bash
+
+clear
+echo "##########################################################"
+toilet -f future "ANONYMOUS INDONESIA" | lolcat
+echo "Author : MrXonee" | lolcat
+echo "##########################################################"
+toilet -f term "   /\_/\ " | lolcat
+toilet -f term " =[ •~• ]= " | lolcat
+toilet -f term "    [\]_" | lolcat
+toilet -f term " /\---------/\      J" | lolcat
+toilet -f term "| |)  •  ~  • |   R • R" | lolcat
+toilet -f term "| |^^^^^^^^^^^| M ••|•• M" | lolcat
+toilet -f term "| |^^^^^^^^^^^] M ••|•• M" | lolcat
+toilet -f term "| |ΩΩ=======ΩΩ]   R • R" | lolcat
+toilet -f term "| |___________|     J" | lolcat
+toilet -f term "\_|_________|_/ GUNAKAN DENGAN BIJAK!!!" | lolcat
+echo "***********************************************************"
+echo "[#] YOU'RE NOT ALONE [#]" | lolcat
+echo "[#] WE ARE ANONYMOUS [#]" | lolcat
+echo "***********************************************************"
+echo "[!] USE WIFI ADAPTER [!]" | lolcat
+echo "***********************************************************"
+echo "$1[1]─>>INSTALL ALL PACKAGE FOR RUNNING TOOLS" | lolcat
+echo "$2[2]─>>TOOLS CONTROL NETWORK EVILLIMITER" | lolcat
+echo "$3[3]─>>TOOLS CRACK WIFI FLUXION" | lolcat
+echo "$4[4]─>>TOOLS" | lolcat
+echo "$5[5]─>>TOOLS" | lolcat
+echo "$6[6]─>>TOOLS" | lolcat
+echo "$7[7]─>>TOOLS" | lolcat
+echo "$8[8]─>>TOOLS" | lolcat
+echo "$9[9]─>>EXIT" | lolcat
+echo "############################################################"
+echo "╭─[MrXonee@root]" | lolcat
+read -p"╰─>> " pil
+
+if [ $pil = 1 ]
+then
+echo $i"Openning... "
+sleep 2
+clear
+sleep 2
+echo "##################################################################################"
+toilet -f future "ANONYMOUS INDONESIA @MrXonee" | lolcat
+echo "**********************************************************************************"
+echo "[!]SEMUA PACKAGE AKAN DI INSTALL MOHON TUNGGU SAMPAI SELESAI" | lolcat
+echo "##################################################################################"
+sleep 1
+apt update
+apt upgrade
+apt install bc
+apt install mdk3
+apt install cowpatty
+apt install dsniff
+apt install php-cgi
+apt install mdk4
+apt install lighttpd
+apt install isc-dhcp-server
+apt install toilet
+apt install figlet
+apt install lolcat
+apt install openssh
+apt install autossh
+apt install php
+apt install python3
+apt install openssl
+apt install git
+echo "##################################################################################"
+echo "SEMUA PACKAGE BERHASIL DI INSTALL" | lolcat
+echo "##################################################################################"
+sleep 5
+bash it.sh
+fi
+
+if [ $pil = 2 ]
+then
+echo $i"Openning..."
+sleep 2
+clear
+sleep 2
+echo "##################################################################################"
+toilet -f future "ANONYMOUS INDONESIA @MrXonee" | lolcat
+echo "**********************************************************************************"
+echo "[!]RUNNING TOOLS EVILLIMITER" | lolcat
+echo "##################################################################################"
+sleep 5
+ifconfig eth0 down
+ifconfig eth1 down
+git clone https://github.com/bitbrute/evillimiter
+cd evillimiter
+python3 setup.py install
+evillimiter
+fi
+
+if [ $pil = 3 ]
+then
+echo $i"Openning..."
+sleep 2
+clear
+sleep 2
+echo "##################################################################################"
+toilet -f future "ANONYMOUS INDONESIA @MrXonee" | lolcat
+echo "**********************************************************************************"
+echo "[!]RUNNING TOOLS FLUXION" | lolcat
+echo "##################################################################################"
+sleep 5
+git clone https://github.com/FluxionNetwork/Fluxion
+cd fluxion
+bash fluxion.sh
+fi
+
+if [ $pil = 4 ]
+then
+echo $i" Coming Soon, need to update "
+fi
+
+if [ $pil = 5 ]
+then
+echo $i"Openning..."
+sleep 1
+echo $cy"TERMUX INDONESIA D4RK-IT Mr.J"
+echo $ku"_________________________________"
+echo $cy"  PHISING MOBILE LEGEND"
+php -S localhost:8080 -t ~/BLACK-IT/Ml
+fi
+
+if [ $pil = 6 ]
+then
+echo $i"Openning... "
+sleep 1
+clear
+sleep 1
+echo $ku"---------------------------------------------------------------------------"
+echo $cy"TERMUX INDONESIA D4RK-IT Mr.J"
+echo $ku"---------------------------------------------------------------------------"
+echo $i"[+]INSTALL METASPLOIT SEDANG DI PROSES"
+echo $i"[+]MOHON SISAKAN PENYIMPANAN INTERNAL (500 MB)"
+echo $i"[+]TUNGGU SAMPAI 30 MENIT UNTUK PENGINSTALLAN"
+echo $ku"---------------------------------------------------------------------------"
+echo $cy"+++++++++++++++[LOADING PAKET METASPLOIT]+++++++++++++++"
+sleep 10
+curl -LO raw.githubusercontent.com/Hax4us/Metasploit_termux/master/metasploit.sh
+chmod 777 metasploit.sh
+./metasploit.sh
+fi
+
+if [ $pil = 7 ]
+then
+php hasil.txt
+fi
+
+if [ $pil = 8 ]
+then
+git clone https://github.com/4NK3R-PRODUCT1ON/FULLDDOS
+cd FULLDDOS
+sh fullddos.sh
+fi
+
+if [ $pil = 15 ]
+then
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+cd RED_HAWK
+chmod 777 rhawk.php
+php rhawk.php
+fi
+
+if [ $pil = 10 ]
+then
+sleep 1
+git clone https://github.com/CiKu370/OSIF.git
+cd OSIF
+chmod 777 osif.py
+python2 osif.py
+fi
+
+if [ $pil = 11 ]
+then
+sleep 1
+git clone https://github.com/amsitlab/smsid-java
+cd smsid
+./install
+smsid
+fi
+
+if [ $pil = 12 ]
+then
+echo $i"Openning..."
+echo $ku"======================="
+echo $cy"ID = Termux"
+echo $cy"PASSWORD = ID"
+echo $ku"======================="
+sleep 1
+git clone https://github.com/storiku/virus
+cd virus
+python2 vbug.py
+fi
+
+if [ $pil = 13 ]
+then
+echo $i"Openning..."
+pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Kali/kali.sh && bash kali.sh
+chmod +x ./start-kali.sh
+./start-kali.sh
+fi
+
+if [ $pil = 14 ]
+then
+pkg install wget
+wget -O install-nethunter-termux https://offs.ec/2MceZWr
+chmod +x install-nethunter-termux
+./install-nethunter-termux
+nethunter kex passwd
+12345678
+nethunter kex &
+fi
+
+if [ $pil = 15 ]
+then
+echo $i"Openning..."
+git clone https://github.com/king-hacking/King-Hacking
+cd King-Hacking
+bash King-Tools.sh
+fi
+
+if [ $pil = 16 ]
+then
+echo $i"Openning..."
+git clone https://github.com/sqlmapproject/sqlmap
+cd sqlmap
+chmod +x *
+python2 sqlmap.py
+fi
+
+if [ $pil = 9 ]
+then
+sleep 1
+echo "============================================="
+echo "[+]TERIMAKASIH SUDAH MEMAKAI TOOLS MrXonee"
+echo "[+]SEE YOU BYE"
+echo "============================================="
+sleep 1
+exit
+fi
