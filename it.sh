1975dcb6a2e1922fd14a5839ab0ba8ef01a07134
MrXonee <114215468+MrXonee@users.noreply.github.com>
2022-11-20 16:11:27 +0000

    Tools Hacking

--- it.sh
+++ it.sh
@@ -20,7 +20,7 @@ toilet -f term "\_|_________|_/ GUNAKAN DENGAN BIJAK!!!" | lolcat
 echo "##################################################################################" | lolcat
 echo "[!] USE A USB WIRELESS ADAPTER [!]" | lolcat
 echo "##################################################################################" | lolcat
-echo "[01]─>>" | lolcat
+echo "[01]─>>INSTALL ALL PACKAGES TO RUN TOOLS" | lolcat
 echo "[02]─>>CONTROL NETWORK WIFI [EVILLIMITER]" | lolcat
 echo "[03]─>>PENETRATION TESTING WIFI [FLUXION]" | lolcat
 echo "[04]─>>VULNERABILITY SCANNING WEBSITE [REDHAWK]" | lolcat
@@ -59,7 +59,38 @@ sleep 2
 echo "##################################################################################" | lolcat
 toilet -f future "ANONYMOUS INDONESIA @MrXonee" | lolcat
 echo "**********************************************************************************" | lolcat
-echo "[!]" | lolcat
+echo "[!]SEMUA PACKAGE AKAN DI INSTALL MOHON TUNGGU SAMPAI SELESAI" | lolcat
+echo "##################################################################################" | lolcat
+sleep 1
+apt update -y
+apt upgrade -y
+apt install bc -y
+apt install mdk3 -y
+apt install cowpatty -y
+apt install dsniff -y
+apt install hostapd -y
+apt install xterm -y
+apt install php-cgi -y
+apt install mdk4 -y
+apt install lighttpd -y
+apt install isc-dhcp-server -y
+apt install toilet -y
+apt install figlet -y
+apt install lolcat -y
+apt install autossh -y
+apt install php -y
+apt install php-curl -y
+apt install php-xml -y
+apt install python3 -y
+apt install openssl -y
+apt install git -y
+apt install slowhttptest -y
+apt install gnome-terminal -y
+apt install etherape -y
+apt install tor torbrowser-launcher -y
+apt install tar tor curl python3 python3-scapy network-manager -y
+echo "##################################################################################" | lolcat
+echo "SEMUA PACKAGE BERHASIL DI INSTALL" | lolcat
 echo "##################################################################################" | lolcat
 sleep 5
 fi
